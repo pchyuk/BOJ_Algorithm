773c2577bc3902ac048385feefbb06fd0690e44d
pchyuk <122423057+pchyuk@users.noreply.github.com>
2024-04-06 00:11:09 +0900

    [Silver III] Title: 주유소, Time: 20 ms, Memory: 3584 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.cc
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -1,34 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int road[100001];
-int oil[100001];
+long long road[100001];
+long long oil[100001];
 
 int main(){
     cin.tie(0);
     cout.tie(0);
     ios_base::sync_with_stdio(0);
-    int n, sum = 0, min = 1000000000;
-    int cost = 0;
+    long long n;
+    long long cost = 0;
     cin >> n;
     for(int i=0; i<n-1; i++){
         cin >> road[i];
-        sum += road[i];
     }
+
     for(int i=0; i<n-1; i++){
-        cin >> oil[i];
-        if(min > oil[i]) min = oil[i];
+        cin >> oil[i];  
     }
-    cin >> oil[n-1];
-    for(int i=0; sum != 0; i++){
-        if(oil[i] == min){
-            cost += oil[i]*sum;
-            sum = 0;
-        }
-        else {
-            cost += oil[i]*road[i];
-            sum -= road[i];
-        }
+    long long min = oil[0];
+
+    for(int i=0; i<n-1; i++){
+        if(min > oil[i]) min = oil[i];
+        cost += min*road[i];
     }
     cout << cost << "\n";
     return 0;
