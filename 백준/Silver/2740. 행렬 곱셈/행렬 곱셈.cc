bba8792fb32ed5ab47afa4cf73ba6f2aa334dede
pchyuk <122423057+pchyuk@users.noreply.github.com>
2024-04-30 22:37:05 +0900

    [Silver V] Title: 행렬 곱셈, Time: 0 ms, Memory: 2140 KB -BaekjoonHub

---
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+int a[101][101];
+int b[101][101];
+int c[101][101];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int n, m, k;
+    cin >> n >> m;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin >> a[i][j];
+        }
+    }
+
+    cin >> m >> k;
+    for(int i=0; i<m; i++){
+        for(int j=0; j<k; j++){
+            cin >> b[i][j];
+        }
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<k; j++){
+            for(int k=0; k<m; k++){
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<k; j++){
+            cout << c[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
