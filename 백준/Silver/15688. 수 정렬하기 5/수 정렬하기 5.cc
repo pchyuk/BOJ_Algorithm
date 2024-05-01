52c6db5018021a499eb0cede6ba26469e257502e
pchyuk <122423057+pchyuk@users.noreply.github.com>
2024-05-02 00:18:11 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 4668 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int arr[1000001];
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    // cout << "\n";
+
+    sort(arr, arr+n);
+
+    for(int i=0; i<n; i++){
+        cout << arr[i] << "\n";
+    }
+    return 0;
+}
