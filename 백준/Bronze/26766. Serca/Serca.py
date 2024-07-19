2b919b5fa04e7d92d6fef377a22459decbe1b752
pchyuk <122423057+pchyuk@users.noreply.github.com>
2024-07-19 15:59:40 +0900

    [Bronze V] Title: Serca, Time: 448 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.py
@@ -0,0 +1,12 @@
+n=int(input())
+for i in range(0,n):
+    print(" @@@   @@@ ")
+    print("@   @ @   @")
+    print("@    @    @")
+    print("@         @")
+    print(" @       @ ")
+    print("  @     @  ")
+    print("   @   @   ")
+    print("    @ @    ")
+    print("     @     ")
+    
