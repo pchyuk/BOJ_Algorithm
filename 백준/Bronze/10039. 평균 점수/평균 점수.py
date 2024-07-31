fc1a5382f507f0c0ddfcb3fb6c1235ab7bb85744
pchyuk <122423057+pchyuk@users.noreply.github.com>
2024-07-31 23:33:11 +0900

    [Bronze IV] Title: 평균 점수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10039. 평균 점수/평균 점수.py
+++ 백준/Bronze/10039. 평균 점수/평균 점수.py
@@ -1,11 +1,10 @@
-a=int(input())
-b=int(input())
-c=int(input())
-d=int(input())
-e=int(input())
-if a<40: a=40
-if b<40: b=40
-if c<40: c=40
-if d<40: d=40
-if e<40: e=40
-print((a+b+c+d+e)//5)
+a=[]
+s=0
+for i in range(5):
+    a.append(0)
+for i in range(5):
+    b=int(input())
+    a[i]=b
+    if a[i]<40: a[i]=40
+    s+=a[i]
+print(s//5)
