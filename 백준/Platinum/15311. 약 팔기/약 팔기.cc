fa50d49c6581f1f10a722335bc422120697de1af
pchyuk <122423057+pchyuk@users.noreply.github.com>
2024-11-13 18:48:33 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  cout << "2000\n";
+  for (int i = 0; i < 1000; i++)
+    cout << "1 ";
+  for (int i = 0; i < 1000; i++)
+    cout << "1000 ";
+  return 0;
+}
