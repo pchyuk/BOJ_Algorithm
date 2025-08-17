2b8d27c0da7521abbea6232a4b6fab3463c559bf
Chanhyuk Park <pch6590@gmail.com>
2025-08-17 21:19:13 +0900

    [Bronze III] Title: 남욱이의 닭장, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11006. 남욱이의 닭장/남욱이의 닭장.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t;
+    int n, m;
+    cin >> t;
+    while(t--){
+        cin >> n >> m;
+        cout << 2*m-n << " " << m - (2*m-n) << "\n";
+    }
+    return 0;
+}
