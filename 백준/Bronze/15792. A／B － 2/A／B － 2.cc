9c41b17a254d46fd6caf70832ff2af47133d51ba
Chanhyuk Park <pch6590@gmail.com>
2025-08-18 23:41:24 +0900

    [Bronze I] Title: A/B - 2, Time: 0 ms, Memory: 2020 KB, Score: 2000 point -BaekjoonHub

---
+++ 백준/Bronze/15792. A／B － 2/A／B － 2.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a, b;
+	cin >> a >> b;
+	cout << int(a / b)<< ".";
+	a = a % b;
+	for (int i = 0; i <= 1000; i++){
+		a *= 10;
+		cout << a / b;
+		a = a - (a / b) * b;
+	}
+}
